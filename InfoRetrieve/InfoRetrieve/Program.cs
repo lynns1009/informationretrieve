d66622d6437fc5aaa058ddc38f2967d9fe958932
Lynns <Lynns1009@users.noreply.github.com>
2018-09-25 18:20:12 +1000

    Start Project
    Start Project

---
+++ InfoRetrieve/InfoRetrieve/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InfoRetrieve
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
