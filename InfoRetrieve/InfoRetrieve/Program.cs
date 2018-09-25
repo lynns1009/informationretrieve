77d338fcc244abf1b21c8d96bb09e9ea82318bf1
Lynns <Lynns1009@users.noreply.github.com>
2018-09-25 22:06:53 +1000

    Learning basic info about C#

--- InfoRetrieve/InfoRetrieve/Program.cs
+++ InfoRetrieve/InfoRetrieve/Program.cs
@@ -2,11 +2,43 @@
 
 namespace InfoRetrieve
 {
+    class Retancles{
+        double length;
+        double width;
+
+        public double GetLength(){
+            length = Convert.ToDouble(Console.ReadLine());
+           
+            return length;
+          
+        }
+        public double GetWidth()
+        {
+            width = Convert.ToDouble(Console.ReadLine());
+            return width;
+        }
+
+        public double GetArea(double length,double width){
+            
+            return length * width;
+        }
+
+    }
+
     class MainClass
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Retancles retancle =new Retancles();
+            var length = retancle.GetLength();
+            var width = retancle.GetWidth();
+            var area = retancle.GetArea(length,width);
+            Console.WriteLine(area);
+
+
+
+
+
         }
     }
 }
